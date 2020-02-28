78ea419ed6ab847a4c00b664978eb653f67e0434
unknown <irisjitomo@yahoo.com>
2020-02-28 12:22:36 -0500

    append ring

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,5 +1,7 @@
 from doubly_linked_list import DoublyLinkedList
 
+from doubly_linked_list import ListNode
+
 
 class RingBuffer:
     def __init__(self, capacity):
@@ -8,7 +10,21 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+            # checks to see if self.storage
+        while self.storage: 
+            # set current node to self.storage.head
+            self.current = self.storage.head 
+            # insert item to linked list
+            self.storage.add_to_head(item)
+            self.current = self.storage.head.next
+            # if capacity is reached 
+            if self.storage > self.capacity:
+                # remove current head node
+                self.storage.remove_from_head(self.current)
+                # add_to_head the item
+                self.storage.add_to_head(item)
+                # set current node to head.next
+                # self.current = self.storage.head.next
 
     def get(self):
         # Note:  This is the only [] allowed
